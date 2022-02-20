3af47b44fabe536ec393fe51f958e7895bec8c80
MaGicSuR <30886720+MaGicSuR@users.noreply.github.com>
2022-02-21 00:19:31 +0100

    Added main GUI
    Added GUI for better cheat navigation

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-__author__ = "MaGicSuR / https://github.com/MaGicSuR"
+__author__ = 'MaGicSuR / https://github.com/MaGicSuR'
+__name__ = 'BetterGo'
+__version__ = '1.4.0'
 
 from memory import *
 from entity import *
@@ -9,35 +11,36 @@ import helper as h
 def glow_esp():
     while True:
         try:
-            for i in range(1, 1024):
-                entity_list = mem.game_handle.read_uint(ent.glow_object() + 0x38 * (i - 1) + 0x4)
-                if entity_list <= 0:
-                    continue
-                if ent.get_dormant(entity_list) == True:
-                    continue
+            if glow_esp_switch:
+                for i in range(1, 1024):
+                    entity_list = mem.game_handle.read_uint(ent.glow_object() + 0x38 * (i - 1) + 0x4)
+                    if entity_list <= 0:
+                        continue
+                    if ent.get_dormant(entity_list) == True:
+                        continue
 
-                if ent.class_id(entity_list) == 40:
-                    if ent.get_team(entity_list) != ent.get_team(lp.local_player()):
-                        mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0x8), 0.47)
-                        mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0xC), 0.24)
+                    if ent.class_id(entity_list) == 40:
+                        if ent.get_team(entity_list) != ent.get_team(lp.local_player()):
+                            mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0x8), 0.47)
+                            mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0xC), 0.24)
+                            mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0x10), 1.0)
+                            mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0x14), 0.6)
+                            mem.game_handle.write_bool(ent.glow_object() + ((0x38 * (i - 1)) + 0x28), True)
+                            mem.game_handle.write_bool(ent.glow_object() + ((0x38 * (i - 1)) + 0x29), False)
+                    elif h.class_id_c4(ent.class_id(entity_list)) or h.class_id_gun(ent.class_id(entity_list)):
+                        mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0x8), 0.95)
+                        mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0xC), 0.12)
+                        mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0x10), 0.54)
+                        mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0x14), 0.6)
+                        mem.game_handle.write_bool(ent.glow_object() + ((0x38 * (i - 1)) + 0x28), True)
+                        mem.game_handle.write_bool(ent.glow_object() + ((0x38 * (i - 1)) + 0x29), False)
+                    elif h.class_id_grenade(ent.class_id(entity_list)):
+                        mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0x8), 1.0)
+                        mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0xC), 1.0)
                         mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0x10), 1.0)
                         mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0x14), 0.6)
                         mem.game_handle.write_bool(ent.glow_object() + ((0x38 * (i - 1)) + 0x28), True)
                         mem.game_handle.write_bool(ent.glow_object() + ((0x38 * (i - 1)) + 0x29), False)
-                elif h.class_id_c4(ent.class_id(entity_list)) or h.class_id_gun(ent.class_id(entity_list)):
-                    mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0x8), 0.95)
-                    mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0xC), 0.12)
-                    mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0x10), 0.54)
-                    mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0x14), 0.6)
-                    mem.game_handle.write_bool(ent.glow_object() + ((0x38 * (i - 1)) + 0x28), True)
-                    mem.game_handle.write_bool(ent.glow_object() + ((0x38 * (i - 1)) + 0x29), False)
-                elif h.class_id_grenade(ent.class_id(entity_list)):
-                    mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0x8), 1.0)
-                    mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0xC), 1.0)
-                    mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0x10), 1.0)
-                    mem.game_handle.write_float(ent.glow_object() + ((0x38 * (i - 1)) + 0x14), 0.6)
-                    mem.game_handle.write_bool(ent.glow_object() + ((0x38 * (i - 1)) + 0x28), True)
-                    mem.game_handle.write_bool(ent.glow_object() + ((0x38 * (i - 1)) + 0x29), False)
         except Exception as err:
             pass
         time.sleep(0.001)
@@ -45,10 +48,11 @@ def glow_esp():
 def auto_pistol(key: int, delay: float):
     while True:
         try:
-            if ent.in_game():
-                if ctypes.windll.user32.GetAsyncKeyState(key) and h.weapon_pistol(ent.active_weapon()):
-                    lp.force_attack(6)
-                    time.sleep(delay)
+            if auto_pistol_switch:
+                if ent.in_game():
+                    if ctypes.windll.user32.GetAsyncKeyState(key) and h.weapon_pistol(ent.active_weapon()):
+                        lp.force_attack(6)
+                        time.sleep(delay)
         except Exception as err:
             pass
         time.sleep(0.01)
@@ -56,16 +60,17 @@ def auto_pistol(key: int, delay: float):
 def trigger_bot(key: int, delay: float):
     while True:
         try:
-            if ent.in_game():
-                if ctypes.windll.user32.GetAsyncKeyState(key):
-                    crosshair = lp.get_crosshair_id()
-                    if crosshair == 0:
-                        continue
+            if trigger_bot_switch:
+                if ent.in_game():
+                    if ctypes.windll.user32.GetAsyncKeyState(key):
+                        crosshair = lp.get_crosshair_id()
+                        if crosshair == 0:
+                            continue
 
-                    target = lp.get_entity_by_crosshair()
-                    if ent.get_team(lp.local_player()) != lp.get_team_by_crosshair(target) and lp.get_health_by_crosshair(target) > 0:
-                        time.sleep(delay)
-                        ent.force_attack(6)
+                        target = lp.get_entity_by_crosshair()
+                        if ent.get_team(lp.local_player()) != lp.get_team_by_crosshair(target) and lp.get_health_by_crosshair(target) > 0:
+                            time.sleep(delay)
+                            ent.force_attack(6)
         except Exception as err:
             pass
         time.sleep(0.001)
@@ -73,14 +78,15 @@ def trigger_bot(key: int, delay: float):
 def bunny_hop(key: int):
     while True:
         try:
-            if lp.get_current_state() == 5:
-                while ctypes.windll.user32.GetAsyncKeyState(key):
-                    if ent.get_flag(lp.local_player()) == 257:
-                        lp.force_jump(5)
-                        time.sleep(0.01)
-                    else:
-                        lp.force_jump(4)
-                        time.sleep(0.01)
+            if bunny_hop_switch:
+                if lp.get_current_state() == 5:
+                    while ctypes.windll.user32.GetAsyncKeyState(key):
+                        if ent.get_flag(lp.local_player()) == 257:
+                            lp.force_jump(5)
+                            time.sleep(0.01)
+                        else:
+                            lp.force_jump(4)
+                            time.sleep(0.01)
         except Exception as err:
             pass
         time.sleep(0.001)
@@ -88,9 +94,10 @@ def bunny_hop(key: int):
 def radar_hack():
     while True:
         try:
-            if ent.in_game():
-                for i in ent.entity_list:
-                    ent.set_is_visible(i, True)
+            if radar_hack_switch:
+                if ent.in_game():
+                    for i in ent.entity_list:
+                        ent.set_is_visible(i, True)
         except Exception as err:
             pass
         time.sleep(1)
@@ -98,14 +105,14 @@ def radar_hack():
 def no_smoke():
     while True:
         try:
-            if ent.in_game():
-                for i in range(1, 1024):
-                    entityList = mem.game_handle.read_uint(ent.glow_object() + 0x38 * (i - 1) + 0x4)
-                    if entityList <= 0:
-                        continue
-                    # if ctypes.windll.user32.GetAsyncKeyState(0x06): # TO:DO add toogle option later
-                    if ent.class_id(entityList) == 157:
-                        ent.set_position(entityList, 0.0, 0.0)
+            if no_smoke_switch:
+                if ent.in_game():
+                    for i in range(1, 1024):
+                        entityList = mem.game_handle.read_uint(ent.glow_object() + 0x38 * (i - 1) + 0x4)
+                        if entityList <= 0:
+                            continue
+                        if ent.class_id(entityList) == 157:
+                            ent.set_position(entityList, 0.0, 0.0)
         except Exception as err:
             pass
         time.sleep(0.01)
@@ -114,16 +121,11 @@ def fov_changer(key_add: int, key_subtract: int, key_normalize: int):
     while True:
         try:
             if ent.in_game():
-                v1 = lp.get_fov()
-                v2 = 0
-                if ctypes.windll.user32.GetAsyncKeyState(key_add):
-                    v2 = v1 + 1
-                    lp.set_fov(v2)
-                elif ctypes.windll.user32.GetAsyncKeyState(key_subtract):
-                    v2 = v1 - 1
-                    lp.set_fov(v2)
-                elif ctypes.windll.user32.GetAsyncKeyState(key_normalize):
-                    lp.set_fov(90)
+                current_fov = lp.get_fov()
+                if current_fov != v1_fov:
+                    lp.set_fov(int(v1_fov))
+                else:
+                    continue
         except Exception as err:
             pass
         time.sleep(0.1)
@@ -132,16 +134,17 @@ def hit_sound(filename: str):
     oldDmg = 0
     while True:
         try:
-            if ent.in_game():
-                damage = ent.get_total_hits(lp.local_player())
-                if ent.get_health(ent.player) <= 0:
-                    continue
-
-                if damage != oldDmg:
-                    oldDmg = damage
-                    if 0 < oldDmg >= 255:
+            if hit_sound_switch:
+                if ent.in_game():
+                    damage = ent.get_total_hits(lp.local_player())
+                    if ent.get_health(ent.player) <= 0:
                         continue
-                    winsound.PlaySound(filename, winsound.SND_ASYNC)
+
+                    if damage != oldDmg:
+                        oldDmg = damage
+                        if 0 < oldDmg >= 255:
+                            continue
+                        winsound.PlaySound(filename, winsound.SND_ASYNC)
         except Exception as err:
             pass
         time.sleep(0.1)
@@ -179,25 +182,63 @@ def spectator_list():
                 pass
         time.sleep(1)
 
-def exit(key: int):
-    while True:
-        if ctypes.windll.user32.GetAsyncKeyState(key):
-            try:
-                print('Exiting...')
-                mem.game_handle.write_int(ent.engine_ptr() + 0x174, -1)
-                lp.set_fov(90)
-            except exception.MemoryWriteError as err:
-                print(err)
-            
-            os._exit(0)
-        time.sleep(0.2)
+def exit():
+    try:
+        print('Exiting...')
+        mem.game_handle.write_int(ent.engine_ptr() + 0x174, -1)
+        lp.set_fov(90)
+    except exception.MemoryWriteError as err:
+        print(err)
+    os._exit(0)
 
-def create_window():
-    h = window.create_window()
+def draw_gui():
+    old_fov = 90
+    global glow_esp_switch, auto_pistol_switch, trigger_bot_switch, bunny_hop_switch, radar_hack_switch, no_smoke_switch, hit_sound_switch, v1_fov
+    spec_list = window.create_spectator_list()
+    main_gui = window.create_main_gui()
+    glow_esp_switch, auto_pistol_switch, trigger_bot_switch, bunny_hop_switch, radar_hack_switch, no_smoke_switch, hit_sound_switch, v1_fov = False, False, False, False, False, False, False, 90
     while True:
-        event, values = h.read(timeout=100)
+        spec_list_event, spec_list_values = spec_list.read(timeout=100)
+        main_gui_event, main_gui_values = main_gui.read(timeout=100)
+        
+        if main_gui_event in (gui.WIN_CLOSED, 'Exit'): # when user closes window
+            exit()
+
+        ### Spectator list ###
         if ent.in_game():
-            h['spec_list'].update(spectator_list())
+            spec_list['spec_list'].update(spectator_list())
+        ### Cheat gui ###
+        if main_gui_values["glow_esp_checkbox"] == True:
+            glow_esp_switch = True
+        else:
+            glow_esp_switch = False
+        if main_gui_values["auto_pistol_checkbox"] == True:
+            auto_pistol_switch = True
+        else:
+            auto_pistol_switch = False
+        if main_gui_values["trigger_bot_checkbox"] == True:
+            trigger_bot_switch = True
+        else:
+            trigger_bot_switch = False
+        if main_gui_values["bunny_hop_checkbox"] == True:
+            bunny_hop_switch = True
+        else:
+            bunny_hop_switch = False
+        if main_gui_values["radar_hack_checkbox"] == True:
+            radar_hack_switch = True
+        else:
+            radar_hack_switch = False
+        if main_gui_values["no_smoke_checkbox"] == True:
+            no_smoke_switch = True
+        else:
+            no_smoke_switch = False
+        if main_gui_values["hit_sound_checkbox"] == True:
+            hit_sound_switch = True
+        else:
+            hit_sound_switch = False
+
+        v1_fov = main_gui_values['fov_value']
+        main_gui['print_fov_value'].update(v1_fov)
 
 def start_threads():
     try:
@@ -210,14 +251,13 @@ def start_threads():
         threading.Thread(target=fov_changer, args=[0x68, 0x62, 0x65], name='fov_changer').start()
         threading.Thread(target=hit_sound, args=['hitsound.wav'], name='hit_sound').start()
         threading.Thread(target=spectator_list, name='spectator_list').start()
-        threading.Thread(target=exit, args=[0x2E], name='exit').start()
-        threading.Thread(target=create_window, name='create_window').start()
+        threading.Thread(target=draw_gui, name='GUI').start()
 
     except Exception as err:
         print(f'Threads have been canceled! Exiting...\nReason: {err}\nExiting...')
         os._exit(0)
 
-if __name__ == '__main__':
+if __name__ == 'BetterGo':
     try:
         start_timer = time.perf_counter()
         mem = Memory(game_handle, client_dll, client_dll_size, engine_dll)
@@ -227,8 +267,9 @@ if __name__ == '__main__':
         ent.entity_loop()
         start_threads()
         stop_timer = time.perf_counter()
+        print(f'Name: {__name__}\nBy: {__author__}\nVersion: {__version__}\n')
         print(f'Initialization took {round((stop_timer - start_timer), 5)} seconds.')
-        print('[NUMPAD 8/5/2] FOV Changer\n[MOUSE4] Auto Pistol\n[MOUSE5] TriggerBot\n[SPACE] BunnyHop\n[DEL] Exit safely from app!')
+        print('[MOUSE4] Auto Pistol\n[MOUSE5] TriggerBot\n[SPACE] BunnyHop')
     except (Exception, KeyboardInterrupt) as err:
         print(f'Failed to initialize!\nReason: {err}\nExiting...')
         os._exit(0)
