12d72edd158bb1c4df02588cbaaf1f60962193e1
OpsecGuy <30886720+OpsecGuy@users.noreply.github.com>
2022-09-15 07:18:03 +0200

    Update 1.5.1 hotfix
    small change

--- app.py
+++ app.py
@@ -554,7 +554,7 @@ def opengl_overlay():
                 # recoil crosshair
                 if dpg.get_value('c_recoil_crosshair'):
                     punch_angle = ent.aim_punch_angle()
-                    if punch_angle.x != 0.0 and lp.get_shots_fired() >= 1:
+                    if punch_angle.x != 0.0 and lp.get_shots_fired() > 1:
                         crosshair_x = x1 - dx * punch_angle.y
                         crosshair_y = y1 - dy * punch_angle.x
                         overlay.draw_lines(crosshair_x, crosshair_y, 1)
